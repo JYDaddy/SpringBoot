8e662f5d938803c9128eed830fe42e4906ecaf79
Kevin <Kevin@KEVIN-ProBook4740s>
2020-06-23 11:20:13 +0900

    initial

---
+++ src/main/java/com/kevin/spring/boot/demoservice/SpringBootDemoApplication.java
@@ -0,0 +1,20 @@
+package com.kevin.spring.boot.demoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+
+	@GetMapping("/hehehe")
+	public String HelloWorld() {
+		return "Hello World";
+	}
+}
